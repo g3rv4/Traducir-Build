601a0837655778e586cf02ede00ad7e350ab55a6
Gervasio Marchand <gmc@gmc.uy>
2018-12-24 16:35:23 -0300

    Clear the cache on deploy

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,4 +1,4 @@
-Set-PSDebug -Trace 1
+Set-PSDebug -Trace 2
 
 $folder = Join-Path '/var/docker-deploy' (Get-ChildItem Env:DOCKER_DEPLOY_FOLDER).Value
 
@@ -9,4 +9,23 @@ mv app "$($folder)/volumes"
 
 docker-compose -f "$($folder)/docker-compose.yml" up -d
 
-Exit $LASTEXITCODE
+# Clear the cache
+$cfEmail = (Get-ChildItem Env:CF_EMAIL).Value
+$cfApiKey = (Get-ChildItem Env:CF_API_KEY).Value
+$cfZone = (Get-ChildItem Env:CF_ZONE).Value
+
+$headers = @{
+    "X-Auth-Email"=$cfEmail;
+    "X-Auth-Key"=$cfApiKey;
+    "Content-Type"="application/json"
+}
+
+$body = @{
+    purge_everything=$true
+} | ConvertTo-Json
+
+$response = Invoke-RestMethod -Uri "https://api.cloudflare.com/client/v4/zoness/$($cfZone)/purge_cache" -Method 'DELETE' -Headers $headers -Body $body
+
+if (!response.success) {
+    Exit 1
+}
