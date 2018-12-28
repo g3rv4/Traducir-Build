aed02a08465e34f550eec9f24739c43f17b415fe
Gervasio Marchand <gmc@gmc.uy>
2018-12-28 15:09:24 -0300

    Have the frontend code at nginx

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,17 +1,29 @@
 Set-PSDebug -Trace 1
 
-$folder = Join-Path '/var/docker-deploy' (Get-ChildItem Env:DOCKER_DEPLOY_FOLDER).Value
+$backendFolder = Join-Path '/var/docker-deploy' (Get-ChildItem Env:DOCKER_DEPLOY_FOLDER).Value
+$frontendFolder = Join-Path '/var/docker-deploy/nginx/volumes/html' (Get-ChildItem Env:DOCKER_DEPLOY_FRONTEND_FOLDER).Value
+$staticHost = (Get-ChildItem Env:STATIC_HOST).Value
 
-docker-compose --no-ansi -f "$($folder)/docker-compose.yml" stop 2>&1
-if($LASTEXITCODE){
+docker-compose --no-ansi -f "$($backendFolder)/docker-compose.yml" stop 2>&1
+if ($LASTEXITCODE) {
     Exit $LASTEXITCODE
 }
 
-rm -rf "$($folder)/volumes/app"
-mv app "$($folder)/volumes"
+# have the links on index.html link to the static host instead of using relative urls (so that we can leverage CF)
+$content = Get-Content app/frontend/index.html
+$content = $content.Replace('src="/', "src=""https://$staticHost/").Replace('href="/', "href=""https://$staticHost/")
+$content > app/frontend/index.html
 
-docker-compose --no-ansi -f "$($folder)/docker-compose.yml" up -d 2>&1
-if($LASTEXITCODE){
+# replace the frontend
+rm -rf $frontendFolder
+cp -r app/frontend $frontendFolder
+
+# replace the backend
+rm -rf "$($backendFolder)/volumes/app"
+mv app "$($backendFolder)/volumes"
+
+docker-compose --no-ansi -f "$($backendFolder)/docker-compose.yml" up -d 2>&1
+if ($LASTEXITCODE) {
     Exit $LASTEXITCODE
 }
 
@@ -20,34 +32,18 @@ Start-Sleep -s 5
 
 # Run migrations
 $instanceNames = (Get-ChildItem Env:INSTANCE_NAMES).Value.Split(',')
-foreach ($instanceName in $instanceNames){
+foreach ($instanceName in $instanceNames) {
     Write-Output "Running migrations on $instanceName"
     docker exec $instanceName curl -f -i http://localhost:5000/app/api/admin/migrate 2>&1
-    if($LASTEXITCODE){
+    if ($LASTEXITCODE) {
         Exit $LASTEXITCODE
     }
 }
 
-# Clear the cache
-$cfEmail = (Get-ChildItem Env:CF_EMAIL).Value
-$cfApiKey = (Get-ChildItem Env:CF_API_KEY).Value
-$cfZone = (Get-ChildItem Env:CF_ZONE).Value
-
-$headers = @{
-    "X-Auth-Email"=$cfEmail;
-    "X-Auth-Key"=$cfApiKey;
-    "Content-Type"="application/json"
-}
-
-$body = @{
-    purge_everything=$true
-} | ConvertTo-Json
-
-Invoke-RestMethod -Uri "https://api.cloudflare.com/client/v4/zones/$($cfZone)/purge_cache" -Method 'DELETE' -Headers $headers -Body $body
-
+# when messing with multiple containers, nginx may get confused... restarting it clears it up
 if ($instanceNames.Count -gt 1) {
     docker-compose --no-ansi -f /var/docker-deploy/nginx/docker-compose.yml restart 2>&1
-    if($LASTEXITCODE){
+    if ($LASTEXITCODE) {
         Exit $LASTEXITCODE
     }
 }
